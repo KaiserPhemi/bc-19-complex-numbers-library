c8d225e1002b0537d875fa447abe25864e78dc2f
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-20 14:46:15 +0100

    test file created

---
+++ app/complex.js
@@ -0,0 +1,12 @@
+/*IIFE Implemented to prevent pollution of the global namespace*/
+(function(){
+	
+	/*To prevent unintended consequences*/
+	'use strict';
+
+	module.exports = {
+		Complex: function(){
+
+		}
+	};
+})();
---
+++ spec/complex.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe("Solve complex numbers", function(){
+
+	describe("Case for no input", function(){
+	
+		it("should return 0 for no input",function(){
+			expect(Complex()).toBe(0);
+		});
+
+	});
+
+	describe("Case for numeric input", function(){
+
+		it("should return ",function(){
+			expect(Complex(3)).toBe(0);
+		});
+	});
+
+
+});
