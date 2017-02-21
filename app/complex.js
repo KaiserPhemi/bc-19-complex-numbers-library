ae7b74443789e6bded1507cf59227ac841c7e602
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-21 12:13:24 +0100

    New methods added to Complex class

--- app/complex.js
+++ app/complex.js
@@ -1,10 +1,37 @@
+#!/usr/bin/env node
 (function(){
 	
 	'use strict';
 
-	module.exports = {
-		Complex: function(){
+	function Complex(real, imaginary){
 
+		/*Checks if real and imaginary parts exist*/
+		this.xAxis = (real == null)? 0 : parseFloat(real);
+		this.yAxis = (imaginary == null)? 0 : parseFloat(imaginary);
+	}
+
+	Complex = {
+
+		addition:function(){
+
+			for(var count = 0; count < arguments.length; count++){
+
+				return new Complex((arguments[count].xAxis+arguments[count+1].xAxis), (arguments[count].yAxis+arguments[count+1].yAxis));
+			}
+		},
+		subtraction:function(complexArg1, complexArg2){
+
+			return new Complex((complexArg1.xAxis-complexArg2.xAxis), (complexArg1.yAxis-complexArg2.yAxis));
+		},
+		multiplication:function(){},
+		division:function(){},
+		conjugate:function(){
+
+			return new Complex(this.xAxis, -this.yAxis);
 		}
+
+		
 	};
+
+	module.exports = Complex;
 })();
--- spec/complex.spec.js
+++ spec/complex.spec.js
@@ -4,23 +4,18 @@ var Complex = require("../app/complex").Complex;
 
 describe("Solve complex numbers", function(){
 
-	describe("Case for no input", function(){
-	
-		it("should return 0 for blank input",function(){
-			expect(Complex()).toBe(0);
-		});
-	});
-
 	describe("Cases for addittion of complex numbers", function(){
 
-		it("should return ",function(){
-			expect(Complex()).toBe(0);
+		it("should return (8, 6) for (3+2i)+(5+4i)",function(){
+			var arg1 = new Complex(3, 2),
+				arg2 = new Complex(5, 4);
+			expect(Complex.addittion(arg1, arg2)).toBe(8, 6);
 		});
 	});
-
+/*
 	describe("Cases for multiplication of complex numbers",function(){
 
-		it("",function(){
+		it("should return ",function(){
 
 		});
 	});
@@ -39,5 +34,5 @@ describe("Solve complex numbers", function(){
 		});
 
 	});
-
+*/
 });
